2c5a5eb647b892855a1042878cd8c35da552c0df
Arturo Herrero <arturo.herrero@gmail.com>
2012-09-07 15:10:19 +0200

    Trying to set a completions for Grails Domain classes

---
+++ GrailsSyntax.py
@@ -0,0 +1,71 @@
+import sublime, sublime_plugin
+import os, string, re
+
+ST2_dir = os.path.dirname(os.path.dirname(os.getcwdu()))
+
+class GrailsSyntaxCommand(sublime_plugin.EventListener):
+    """ Attempts to set Grails Syntax when appropriate. """
+
+
+    def __init__(self):
+        super(GrailsSyntaxCommand, self).__init__()
+        self.path = None
+        self.name = None
+        self.ext  = None
+        self.view = None
+
+
+    def on_load(self, view):
+        self.check_syntax(view)
+
+
+    def on_post_save(self, view):
+        self.check_syntax(view)
+
+
+    def check_syntax(self, view):
+        self.view      = view
+        self.file_name = view.file_name()
+    
+        if not self.file_name: # buffer has never been saved
+            return
+
+        self.reset_cache_variables()
+
+        if not self.ext == '.groovy':
+            print "Not Groovy file - GrailsSyntax exiting"
+            return
+
+        if self.is_domain():
+            self.set_syntax('Python Django', 'Djaneiro')
+            return
+        else:
+            self.set_syntax('Python', 'Python')
+            return
+
+
+    def is_django(self):
+        if self.view.find('from django',0,sublime.IGNORECASE) > -1:
+            return True
+        return False
+
+
+    def reset_cache_variables(self):
+        self.path = os.path.dirname(self.file_name)
+        self.name = os.path.basename(self.file_name).lower()
+        self.name, self.ext = os.path.splitext(self.name)
+
+
+    def set_syntax(self, syntax, path = None):
+        if path is None:
+            path = syntax
+
+        new_syntax = 'Packages/' + path + '/' + syntax + '.tmLanguage'
+
+        if not os.path.exists(os.path.join(ST2_dir, new_syntax)):
+            new_syntax = 'Packages/' + path + '/Syntaxes/' + syntax + '.tmLanguage'
+        current_syntax = self.view.settings().get('syntax')
+
+        if current_syntax != new_syntax:
+            self.view.settings().set('syntax', new_syntax)
+            print "Switched syntax to: " + syntax
