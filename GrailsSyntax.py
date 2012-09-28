38db5f67c5e807bb13dd2df3c581bcd7dbd08fb1
Arturo Herrero <arturo.herrero@gmail.com>
2012-09-28 11:27:52 +0200

    Refactor set Grails syntax when found domain and controller classes

--- GrailsSyntax.py
+++ GrailsSyntax.py
@@ -1,7 +1,6 @@
 import sublime, sublime_plugin
 import os, string, re
 
-ST2_dir = os.path.dirname(os.path.dirname(os.getcwdu()))
 
 class GrailsSyntaxCommand(sublime_plugin.EventListener):
     """ Attempts to set Grails Syntax when appropriate. """
@@ -30,13 +29,15 @@ class GrailsSyntaxCommand(sublime_plugin.EventListener):
         if not self.file_name: # buffer has never been saved
             return
 
-        self.reset_cache_variables()
+        self.set_file_variables()
 
         if not self.ext == '.groovy':
             return
 
         if self.is_domain():
             self.set_syntax('GrailsDomain', 'Grails/Domain')
+        elif self.is_controller():
+            self.set_syntax('GrailsController', 'Grails/Controller')
         else:
             self.set_syntax('Groovy', 'Groovy')
 
@@ -47,21 +48,22 @@ class GrailsSyntaxCommand(sublime_plugin.EventListener):
         return False
 
 
-    def reset_cache_variables(self):
+    def is_controller(self):
+        if 'grails-app/controller' in self.path:
+            return True
+        return False
+
+
+    def set_file_variables(self):
         self.path = os.path.dirname(self.file_name)
         self.name = os.path.basename(self.file_name).lower()
         self.name, self.ext = os.path.splitext(self.name)
 
 
-    def set_syntax(self, syntax, path = None):
-        if path is None:
-            path = syntax
-
+    def set_syntax(self, syntax, path):
         new_syntax = 'Packages/' + path + '/' + syntax + '.tmLanguage'
-
-        if not os.path.exists(os.path.join(ST2_dir, new_syntax)):
-            new_syntax = 'Packages/' + path + '/Syntaxes/' + syntax + '.tmLanguage'
-        current_syntax = self.view.settings().get('syntax')
+        self.view.set_syntax_file(new_syntax)
+      current_syntax = self.view.settings().get('syntax')
 
         if current_syntax != new_syntax:
             self.view.settings().set('syntax', new_syntax)
